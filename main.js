cc40e789c070390b2741bc49a0798664ab69f1f6
fatima <162475613+fatimaRiaz531@users.noreply.github.com>
2024-04-07 20:16:57 +0500

    Add files via upload
    BMI CALCULATOR

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+let weightInkg = 50;
+let heightInMeters = 1.60;
+let bmi = weightInkg / (heightInMeters * heightInMeters);
+console.log(`Your BMI ${bmi}`);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let weightInkg =50;
+let heightInMeters = 1.60;
+let bmi = weightInkg / (heightInMeters* heightInMeters);
+console.log(`Your BMI ${bmi}`);
